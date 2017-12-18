1466350e48d9222e464536a4bca214863c6c9806
Harijs Veveris <harijs.veveris@gig.com>
2017-12-18 21:43:45 +0100

    update with phpmailer

--- php/send.php
+++ php/send.php
@@ -1,7 +1,16 @@
 <?php
 
+    require 'PHPMailer.php';
+
     if (isset($_POST['to']) && isset($_POST['from']) && isset($_POST['greeting']) && isset($_POST['img'])) {
 
+        $lang = $_POST['lang'] || 'lv';
+        $promoImgSrc = "https://squalio.com/christmas-greeting/email/ginger-receiver-" . $lang . ".jpg";
+        $to = $_POST['to'];
+        $from = $_POST['from'];
+        $greeting = $_POST['greeting'];
+
+        // save image
         define('UPLOAD_DIR', '../img/');
         $img = $_POST['img'];
         $img = str_replace('data:image/png;base64,', '', $img);
@@ -10,57 +19,48 @@
         $file = UPLOAD_DIR . uniqid() . '.png';
         $success = file_put_contents($file, $data);
 
-        $lang = $_POST['lang'] || 'lv';
-        $to = $_POST['to'];
-        $from = $_POST['from'];
-        $greeting = $_POST['greeting'];
-        $subject = "Ziemassvētku sveiciens!";
-        $url = "https://squalio.com/christmas-greeting/?from=" . $from . "&greeting=" . $greeting . "&img=" . $file;
-        $promoImgSrc = "https://squalio.com/christmas-greeting/email/ginger-receiver-" . $lang . ".jpg";
 
+        //send email
+        $mail = new PHPMailer();
+        
+        $mail->From = "info@cocoracoffee.es";
+        $mail->FromName = $_POST['from'];
+        $mail->Sender = "info@cocoracoffee.es";
+        $mail->AddAddress($to);
+        $mail->Subject = "Ziemassvētku sveiciens";
+
+        $mail->IsHTML(true);
+        $mail->AltBody = "This is text only alternative body.";
+        $mail->AddEmbeddedImage($promoImgSrc, 'promoImage', $promoImgSrc);
+        
+        $url = "https://squalio.com/christmas-greeting/?from=" . $from . "&greeting=" . $greeting . "&img=" . $file;
         $htmlContent = '
-                <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-                <html xmlns="http://www.w3.org/1999/xhtml">
-                    <head>
-                        <title>Happy Holidays from DPA SQUALIO</title>
-                        <meta name="viewport" content="width=1920">
-                        <meta content="text/html; charset=UTF-8" http-equiv="content-type">
-                        <meta http-equiv="Content-Type">
-                    </head>
-                    <body style="background-color: #FFFFFF; margin: 0; padding: 0;" bgcolor="#FFFFFF">
-                        <table cellspacing="0" border="0" align="center" cellpadding="0" width="100%">
+                <body style="background-color: #FFFFFF; margin: 0; padding: 0;" bgcolor="#FFFFFF">
+                    <table cellspacing="0" border="0" align="center" cellpadding="0" width="100%">
                         <tr>
                             <td align="center" bgcolor="#FFFFFF" style="background-color: #FFFFFF; padding:0px; margin:0px;">
                                 <a href="' . $url . '" target="_blank">
-                                    <img src="' . $promoImgSrc . '" width="1920" height="1080" alt="Click here!" title="Click here!" usemap="#link" style="font-family: Georgia; color: #FFFFFF; font-size: 18px;" />
+                                    <img src=\'cid:promoImage\' width="1920" height="1080" alt="Click here!" title="Click here!" usemap="#link" style="font-family: Georgia; color: #FFFFFF; font-size: 18px;" />
                                 </a>
                             </td>
                         </tr>
-                        </table>
-                    </body>
-                </html>
+                    </table>
+                </body>            
             ';
 
-        // Set content-type header for sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $mail->Body = $htmlContent;
+        
 
-        // Additional headers
-        $headers .= 'From: Cocóra Coffee Roasters<info@cocoracoffee.es>' . "\r\n";
-        //$headers .= 'Cc: welcome@example.com' . "\r\n";
-        //$headers .= 'Bcc: welcome2@example.com' . "\r\n";
-
-        // Send email
-        if(mail($to, $subject, $htmlContent, $headers)):
-            $response = 'ok';
-        else:
-            $response = 'error';
-        endif;
+        try {
+            $mail->send();
+            echo 'ok';
+        } catch (Exception $e) {
+            echo 'error';
+        }
 
     } else {
-        $response = 'error';
+        echo 'error';
     }
 
-    echo $response;
 
 ?>
