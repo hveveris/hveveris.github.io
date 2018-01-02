cbbddf5d6ea1ad884403102f0a5185d125a78178
Harijs Veveris <harijs.veveris@gig.com>
2018-01-02 15:51:50 +0100

    upd

--- php/send.php
+++ php/send.php
@@ -1,66 +1,85 @@
 <?php
 
-    require 'PHPMailer.php';
+if (isset($_POST['to']) && isset($_POST['from']) && isset($_POST['greeting']) && isset($_POST['img'])) {
 
-    if (isset($_POST['to']) && isset($_POST['from']) && isset($_POST['greeting']) && isset($_POST['img'])) {
+    $lang = $_POST['lang'] == 'en' ? 'en' : 'lv';
+    $to = $_POST['to'];
+    $from = urlencode($_POST['from']);
+    $greeting = urlencode($_POST['greeting']);
+    $subject = 'SQUALIO sveic Tevi gada gaišākajos svētkos un novēl saticību un mieru katrās mājās!';
+    $fromAddress = 'SQUALIO <squalio@squalio.com>';
+    $altText = 'Gaišām pārslām virmo Ziemassvētku gaidīšanas sajūta. Kāds ļoti īpašs rūķis sirsnīgi sveic Tevi svētkos! Atver apsveikumu un apskati tieši Tev veidotu sveicienu! Spied šeit!';
 
-        $lang = $_POST['lang'] || 'lv';
-        $promoImgSrc = "https://squalio.com/christmas-greeting/email/ginger-receiver-" . $lang . ".jpg";
-        $to = $_POST['to'];
-        $from = $_POST['from'];
-        $greeting = $_POST['greeting'];
-
-        // save image
-        define('UPLOAD_DIR', '../img/');
-        $img = $_POST['img'];
-        $img = str_replace('data:image/png;base64,', '', $img);
-        $img = str_replace(' ', '+', $img);
-        $data = base64_decode($img);
-        $file = UPLOAD_DIR . uniqid() . '.png';
-        $success = file_put_contents($file, $data);
-
-
-        //send email
-        $mail = new PHPMailer();
-        
-        $mail->From = "info@cocoracoffee.es";
-        $mail->FromName = $_POST['from'];
-        $mail->Sender = "info@cocoracoffee.es";
-        $mail->AddAddress($to);
-        $mail->Subject = "Ziemassvētku sveiciens";
-
-        $mail->IsHTML(true);
-        $mail->AltBody = "This is text only alternative body.";
-        $mail->AddEmbeddedImage($promoImgSrc, 'promoImage', $promoImgSrc);
-        
-        $url = "https://squalio.com/christmas-greeting/?from=" . $from . "&greeting=" . $greeting . "&img=" . $file;
-        $htmlContent = '
-                <body style="background-color: #FFFFFF; margin: 0; padding: 0;" bgcolor="#FFFFFF">
-                    <table cellspacing="0" border="0" align="center" cellpadding="0" width="100%">
-                        <tr>
-                            <td align="center" bgcolor="#FFFFFF" style="background-color: #FFFFFF; padding:0px; margin:0px;">
-                                <a href="' . $url . '" target="_blank">
-                                    <img src=\'cid:promoImage\' width="1920" height="1080" alt="Click here!" title="Click here!" usemap="#link" style="font-family: Georgia; color: #FFFFFF; font-size: 18px;" />
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                </body>            
-            ';
-
-        $mail->Body = $htmlContent;
-        
-
-        try {
-            $mail->send();
-            echo 'ok';
-        } catch (Exception $e) {
-            echo 'error';
-        }
-
-    } else {
-        echo 'error';
+    if ($lang == 'en') {
+        $subject = $altText = "SQUALIO wishes you and your family bright, prosperous and happiest time ahead!";
     }
 
+
+    // save image
+    define('UPLOAD_DIR', '../img/');
+    $img = $_POST['img'];
+    $img = str_replace('data:image/png;base64,', '', $img);
+    $img = str_replace(' ', '+', $img);
+    $data = base64_decode($img);
+    $fileName = uniqid() . '.png';
+    $file = UPLOAD_DIR . $fileName;
+    $success = file_put_contents($file, $data);
+
+    $url = "https://www.squalio.com/christmas-greeting/?from=" . $from . "&greeting=" . $greeting . "&img=" . $fileName . "&lang=".$lang;
+    $promoImgSrc = "https://www.squalio.com/christmas-greeting/email/images/ginger-receive-" . $lang ."_";
+    $r = time();
+
+    $fontSize = strlen($_POST['from']) > 26 ? 10 : 14;
+    $htmlContent = '
+        <html>
+        <head>
+        <title>'.$subject.'</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        </head>
+        <body style="background-color: #f2f2f2; margin: 0; padding: 0;">
+        <table id="Table_01" width="1024" height="576" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+                <td colspan="3">
+                <a href="' . $url . '" target="_blank">
+                    <img src="' . $promoImgSrc . '01.png"'.$r.' style="line-height: 0; vertical-align: middle;" width="1024" height="159" alt="' . $altText . '"></a></td>
+            </tr>
+            <tr>
+                <td rowspan="2">
+                <a href="' . $url . '" target="_blank">
+                    <img src="' . $promoImgSrc . '02.png"'.$r.' style="line-height: 0; vertical-align: middle;" width="282" height="417" alt="' . $altText . '"></a></td>
+                <td width="90" height="45" bgColor="#a2845a">
+                    <div width="90" height="45" style="font-family: Helvetica; font-size:'.$fontSize.'px"><font color="#ffeda9">'.$_POST['from'].'</font></td>                    
+                <td rowspan="2">
+                <a href="' . $url . '" target="_blank">
+                    <img src="' . $promoImgSrc . '04.png"'.$r.' style="line-height: 0; vertical-align: middle;" width="652" height="417" alt="' . $altText . '"></a></td>
+            </tr>
+            <tr>
+                <td>
+                <a href="' . $url . '" target="_blank">
+                    <img src="' . $promoImgSrc . '05.png"'.$r.' style="line-height: 0; vertical-align: middle;" width="90" height="372" alt="' . $altText . '"></a></td>
+            </tr>
+        </table>
+        </body>
+        </html>
+    ';
+
+
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+     
+    // Create email headers
+    $headers .= 'From: '.$fromAddress."\r\n" . 'Reply-To: '.$fromAddress."\r\n" . 'X-Mailer: PHP/' . phpversion();
+     
+     
+    // Sending email
+    if(mail($to, $subject, $htmlContent, $headers)){
+        echo 'ok';
+    } else{
+        echo 'error1';
+    }
+} else {
+    echo 'error2';
+}
 
 ?>
